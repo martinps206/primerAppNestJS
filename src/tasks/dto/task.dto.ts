051c22321d945c76be182c8345db9e4513f9daf3
martinps206 <martinps_cc@hotmail.com>
2024-07-15 23:01:07 -0300

    validar datos ingresados

--- src/tasks/dto/task.dto.ts
+++ src/tasks/dto/task.dto.ts
@@ -1,12 +1,26 @@
-import { TaskStatus } from "../task.entity";
+import { TaskStatus } from '../task.entity';
+import { IsOptional, IsIn, IsString, IsNotEmpty, Min, MinLength } from "class-validator";
 
 export class CreateTaskDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
     title: string;
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(5)
     description: string;
 }
 
 export class UpdateTaskDto {
+    @IsString()
+    @IsOptional()
     title?: string;
+    @IsString()
+    @IsOptional()
     description?: string;
+    @IsString()
+    @IsOptional()
+    @IsIn([TaskStatus.DONE, TaskStatus.IN_PROGRESS, TaskStatus.PENDING])
     status?: TaskStatus;
 }
